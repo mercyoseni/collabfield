87111ee4bd3e0540018f6e79e0eab5d4e75cb3b8
Halimat Mercy Oseni <mercy.oseni@andela.com>
2019-07-18 20:08:24 +0100

    Update specs to fix stub related deprecation warnings

--- spec/helpers/navigation_helper_spec.rb
+++ spec/helpers/navigation_helper_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe NavigationHelper, type: :helper do
   describe '#collapsible_links_partial_path' do
     context 'signed in user' do
-      before { helper.stub(:user_signed_in?).and_return(true) }
+      before do
+        allow(helper).to receive(:user_signed_in?).and_return(true)
+      end
 
       it "returns signed_in_links partial's path" do
         expect(helper.collapsible_links_partial_path).to eq(
@@ -13,7 +15,9 @@ RSpec.describe NavigationHelper, type: :helper do
     end
 
     context 'non-signed in user' do
-      before { helper.stub(:user_signed_in?).and_return(false) }
+      before do
+        allow(helper).to receive(:user_signed_in?).and_return(false)
+      end
 
       it "returns non_signed_in_links partial's path" do
         expect(helper.collapsible_links_partial_path).to eq(

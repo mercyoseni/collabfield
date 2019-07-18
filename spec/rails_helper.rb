2493b91626e664c58582baf1130b02e3e3363d31
Halimat Mercy Oseni <mercy.oseni@andela.com>
2019-07-18 21:03:45 +0100

    Install ffaker gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 require 'factory_bot_rails'
 require 'capybara/rspec'
+require 'ffaker'
 
 begin
   ActiveRecord::Migration.maintain_test_schema!

111387429266ec2cdff7269678a9048afd987736
Halimat Mercy Oseni <mercy.oseni@andela.com>
2019-07-19 20:59:29 +0100

    Add specs for scopes

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -20,5 +20,35 @@ RSpec.describe Post, type: :model do
 
       expect(Post.all).to eq([second_post, first_post])
     end
+
+    it 'by_category scope gets posts by particular category' do
+      category = create(:category)
+      create(:post, category: category)
+      create_list(:post, 2)
+      posts = Post.by_category(category.branch, category.name)
+
+      expect(posts.count).to eq(1)
+      expect(posts.first.category.name).to eq(category.name)
+    end
+
+    it 'by_branch scope gets posts by particular branch' do
+      category = create(:category)
+      create(:post, category: category)
+      create_list(:post, 2)
+      posts = Post.by_branch(category.branch)
+
+      expect(posts.count).to eq(1)
+      expect(posts.first.category.branch).to eq(category.branch)
+    end
+
+    it 'search finds a matching post' do
+      post = create(:post, title: 'awesome title', content: 'great content ' * 5)
+      create_list(:post, 2, title: ('a'..'c' * 2).to_a.shuffle.join)
+
+      expect(Post.search('awesome').count).to eq(1)
+      expect(Post.search('awesome').first).to eq(post)
+      expect(Post.search('great').count).to eq(1)
+      expect(Post.search('great').first).to eq(post)
+    end
   end
 end

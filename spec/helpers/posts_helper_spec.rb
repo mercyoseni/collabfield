672551ec68db90e02ee3ee2d8bfa607d548dce72
Halimat Mercy Oseni <mercy.oseni@andela.com>
2019-07-19 18:32:13 +0100

    Add specs for PostsHelper methods

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -1,15 +1,73 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the PostsHelper. For example:
-#
-# describe PostsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe PostsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create_new_post_partial_path' do
+    context 'when user is signed in' do
+      before do
+        allow(helper).to receive(:user_signed_in?).and_return(true)
+      end
+
+      it "returns a signed_in partial's path" do
+        expect(helper.create_new_post_partial_path). to eq(
+          'posts/branch/create_new_post/signed_in'
+        )
+      end
+    end
+
+    context 'when user is NOT signed in' do
+      before do
+        allow(helper).to receive(:user_signed_in?).and_return(false)
+      end
+
+      it "returns a not_signed_in partial's path" do
+        expect(helper.create_new_post_partial_path). to eq(
+          'posts/branch/create_new_post/not_signed_in'
+        )
+      end
+    end
+  end
+
+  describe '#all_categories_button_partial_path' do
+    context 'when category params is empty' do
+      it "returns an all_selected partial's path" do
+        controller.params[:category] = ''
+
+        expect(helper.all_categories_button_partial_path).to eq(
+          'posts/branch/categories/all_selected'
+        )
+      end
+    end
+
+    context 'when category params is NOT empty' do
+      it "returns an all_not_selected partial's path" do
+        controller.params[:category] = 'category'
+
+        expect(helper.all_categories_button_partial_path).to eq(
+          'posts/branch/categories/all_not_selected'
+        )
+      end
+    end
+  end
+
+  describe '#no_posts_partial_path' do
+    context 'when there is post' do
+      it "returns an empty partial's path" do
+        assign(:posts, [1])
+
+        expect(helper.no_posts_partial_path).to eq(
+          'shared/empty_partial'
+        )
+      end
+    end
+
+    context 'when there is NO post' do
+      it "returns a no_posts partial's path" do
+        assign(:posts, [])
+
+        expect(helper.no_posts_partial_path).to eq(
+          'posts/branch/no_posts'
+        )
+      end
+    end
+  end
 end

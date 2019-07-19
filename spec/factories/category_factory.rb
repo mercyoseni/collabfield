2951a18dcb0e2563120e4151859ff3b3e7eefae3
Halimat Mercy Oseni <mercy.oseni@andela.com>
2019-07-19 23:13:48 +0100

    Set branch to use defaults - Any of hobby, team or study

--- spec/factories/category_factory.rb
+++ spec/factories/category_factory.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :category do
     name { FFaker::Name.unique.name }
-    sequence(:branch) { |n| "branch #{ n }" }
+    branch { %w(hobby team study).sample }
   end
 end

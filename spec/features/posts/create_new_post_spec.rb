14c967001120f92361cb1f5446c00709b2c3fa5f
Halimat Mercy Oseni <mercy.oseni@andela.com>
2019-07-22 14:51:01 +0100

    Add specs for unsuccessful post creation

--- spec/features/posts/create_new_post_spec.rb
+++ spec/features/posts/create_new_post_spec.rb
@@ -16,12 +16,32 @@ RSpec.feature 'Create a new post', type: :feature do
       fill_in 'post[title]', with: 'a' * 20
       fill_in 'post[content]', with: 'a' * 20
 
-      select 'category', from: 'post[category_id]'
-
       click_on 'Create a post'
 
       expect(page).to have_selector('h3', text: 'a' * 20)
       expect(page).to have_content("Posted by #{ user.name }")
+    end
+
+    # If the front-end validations is disabled in dev tools
+    # TODO: Only passes in driver mode at the moment, find a fix
+    scenario 'NOT successful', :js, driver: :selenium do
+      create(:category, name: 'category', branch: branch)
+
+      visit send("#{ branch }_posts_path")
+
+      click_on 'Create a new post'
+
+      # Remove required attribute on title and content
+      page.execute_script("$('#post_title').removeAttr('required')")
+      page.execute_script("$('#post_content').removeAttr('required')")
+
+      click_on 'Create a post'
+
+      expect(page).to have_content('The form contains 4 errors.')
+      expect(page).to have_content("Title can't be blank")
+      expect(page).to have_content('Title is too short (minimum is 5 characters)')
+      expect(page).to have_content("Content can't be blank")
+      expect(page).to have_content('Content is too short (minimum is 20 characters)')
     end
   end
 
